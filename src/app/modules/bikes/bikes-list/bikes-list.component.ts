e0304e79649ebf1222d69d63dcc45fed72a102e9
Yohon Jairo Bravo Castro <bravo2008@misena.edu.co>
2020-06-04 22:33:56 -0500

    fix error

--- src/app/modules/bikes/bikes-list/bikes-list.component.ts
+++ src/app/modules/bikes/bikes-list/bikes-list.component.ts
@@ -11,47 +11,6 @@ export class BikesListComponent implements OnInit {
 
   bikesList: IBike[];
 
-<<<<<<< HEAD
-  deleteStatus = false;
-  sale = {
-    idClient: 0
-  };
-
-  totalRecords: any;
-  pageSize = 10;
-  pageNumber = 0;
-  constructor(private bikesService: BikesService) {
-   }
-
-  ngOnInit() {
-   this.loadingBikesPage(this.pageNumber, this.pageSize);
-
-  }
-
-  loadingBikesPage(pageNumber: number, pageSize: number ): void {
-    this.bikesService.queryBikes({
-      pageSize: pageSize,
-      pageNumber: pageNumber
-    })
-    .subscribe((res: any) => {
-      this.bikesList = res.content;
-      this.totalRecords = res.totalElements;
-    });
-  }
-  deleteItem(id: string) {
-    this.bikesService.delete(id)
-    .subscribe(res => {
-      console.log("Item Delete ok");
-      this.deleteStatus = true;
-      this.ngOnInit();
-    });
-  }
-
-  paginate(event: any): void {
-    console.warn('Event Paginate ',event);
-    this.loadingBikesPage(event.page, event.rows);
-  }
-=======
   pageSize = 10;
   pageNumber = 0;
   totalRecords: any;
@@ -79,5 +38,4 @@ export class BikesListComponent implements OnInit {
   }
 
  
->>>>>>> develop
 }
